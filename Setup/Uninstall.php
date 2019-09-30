23fffe5f58911e64a0db8bd3aef0575dd7d2b041
Aurelien Jourquin <aurelien@growzup.com>
2019-09-29 21:38:55 -0400

    Delete added column in customer_entity table

---
+++ Setup/Uninstall.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+/**
+ * @author    Aurélien Jourquin <aurelien@growzup.com>
+ * @link      http://www.ajourquin.com
+ */
+
+namespace Ajourquin\DisableCustomer\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class Uninstall implements UninstallInterface
+{
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context): void
+    {
+        $setup->getConnection()->dropColumn(
+            $setup->getTable('is_disabled'),
+            'customer_entity'
+        );
+    }
+}
